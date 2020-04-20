1a95aa709d3188a1e2d09851d9827d6a301f8766
Tafadzwanashe <48951981+Tafadzwanashe@users.noreply.github.com>
2020-04-20 21:39:39 +0300

    Create frequencyApp.cpp

---
+++ frequencyApp.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+
+using namespace std;
+
+struct frequency{
+
+                char letter;
+
+                int frequency;
+
+}  ;
+
+int main{
+
+cout<<"Please enter Menu Option \n 1) "
+
+
+
+
+
+
+return 0;
+}
