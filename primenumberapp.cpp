73650d2425057fffb8dfc478e59fd08b3a321bde
Tafadzwanashe <48951981+Tafadzwanashe@users.noreply.github.com>
2020-05-13 11:45:19 +0300

    Update primenumberapp.cpp

--- primenumberapp.cpp
+++ primenumberapp.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 
 void tryDivideNumber(int number);
+void method1Composite(int number);
 
 int main(){
 
@@ -21,15 +22,47 @@ tryDivideNumber(number);
 
 void tryDivideNumber(int number){
 	
-	int answer;
+
 	
-	for (int i=2; i<number; i++){
+	for (int i=2; i<number+1; i++){
+		
+		
+		if(i == number){
+			
+			cout<<number<<" is a Prime number";
+			break;
+		}
 		
 		if( number%i ==0){
 			
-			cout<<i << "\t";
+			method1Composite(number);
+			break;
 		}
 		
 	}
 	
+}
+
+void method1Composite(int number){
+	
+	
+	cout<<number<<" is a composite number and factors are -> ";
+	
+	int answer=0, count=0;
+	
+	for (int i=2; i<number+1; i++){
+		
+		count++;
+		if(number==i){
+			cout<<i;
+			break;
+		}
+		if( number%i ==0){
+			
+			cout<<i << ",";
+		}
+		
+	}
+	
+	cout<<"\nWith 1st method number of iterations is: "<<count;
 }
