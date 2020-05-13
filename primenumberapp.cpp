1e638dc5efbfdb5b071af7a35c35525be64d5f5d
Tafadzwanashe <48951981+Tafadzwanashe@users.noreply.github.com>
2020-05-13 10:42:54 +0300

    Add files via upload

---
+++ primenumberapp.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<cstdlib>
+#include<ctime>
+#include<string.h>
+#include <stdio.h>
+#include <ctype.h>
+
+
+using namespace std;
+
+void tryDivideNumber(int number);
+
+int main(){
+
+int number;
+cout<<"Please give a number-> ";
+cin>> number;
+
+
+}
+
+void tryDivideNumber(int number){
+	
+}
