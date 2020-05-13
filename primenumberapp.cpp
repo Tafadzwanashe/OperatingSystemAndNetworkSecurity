8cf245f9f29f7fc6c699a860d4400ef761d7942d
Tafadzwanashe <48951981+Tafadzwanashe@users.noreply.github.com>
2020-05-13 12:27:44 +0300

    Update primenumberapp.cpp

--- primenumberapp.cpp
+++ primenumberapp.cpp
@@ -1,6 +1,7 @@
 #include<iostream>
 #include<cstdlib>
 #include<ctime>
+#include<cmath>
 #include<string.h>
 #include <stdio.h>
 #include <ctype.h>
@@ -10,6 +11,7 @@ using namespace std;
 
 void tryDivideNumber(int number);
 void method1Composite(int number);
+void method2(int x );
 
 int main(){
 
@@ -17,19 +19,21 @@ int number;
 cout<<"Please give a number-> ";
 cin>> number;
 tryDivideNumber(number);
+method2(number);
 
 }
 
 void tryDivideNumber(int number){
 	
-
+	int count=0;
 	
 	for (int i=2; i<number+1; i++){
 		
-		
+		count++;
 		if(i == number){
 			
-			cout<<number<<" is a Prime number";
+			cout<<number<<" is a Prime number and factors are -> "<<i;
+			cout<<"\nWith 1st method number of iterations is: "<<count<<endl;
 			break;
 		}
 		
@@ -64,5 +68,26 @@ void method1Composite(int number){
 		
 	}
 	
-	cout<<"\nWith 1st method number of iterations is: "<<count;
+	cout<<"\nWith 1st method number of iterations is: "<<count<<endl;
+}
+
+void method2(int n ){
+	
+	int i=2,b,x, count;
+	x = n;
+	b = sqrt(x)	;
+	while(x>1 && i<=b){
+		count++;
+		while((x%i)==0) {
+			cout<<i;
+			x= x/i;
+			b= sqrt(x);
+		}
+		
+		i = i+1;
+	}
+	if (x>1){
+		cout<<"\n"<<x;
+	}
+	
 }
