afb00c07d86bf5dcdbe514c02b8cd87b7f780a1f
Tafadzwanashe <48951981+Tafadzwanashe@users.noreply.github.com>
2020-05-27 20:01:49 +0300

    Update sieveOfEratosthenes.cpp

--- sieveOfEratosthenes.cpp
+++ sieveOfEratosthenes.cpp
@@ -10,6 +10,8 @@
 using namespace std;
 
 void createList(int numbers[10000], int upperLimit);
+void findPrimeNumbers(int numbers[10000], int upperLimit);
+void displayPrimeNumbers(int numbers[10000], int upperLimit);
 
 
 int main(){
@@ -22,6 +24,8 @@ again=0;
 cout<<"Please enter upper limit number -> ";
 cin>> upperLimit;
 createList(numbers, upperLimit);
+findPrimeNumbers(numbers, upperLimit);
+displayPrimeNumbers(numbers, upperLimit);
 
  cout<<"\n\nPress 5 to try again or other numbers to exit: ";
  cin>>again;
@@ -34,11 +38,51 @@ void createList(int numbers[10000], int upperLimit){
 //	size = upperLimit;
 
 	
-	for(int i=2; i< upperLimit; i++){
+	for(int i=0; i< upperLimit; i++){
 		
-		numbers[i] = i;
+		numbers[i] = i+2;
 		cout<< numbers[i] <<"_";
+		if (numbers[i]== upperLimit){
+			break;
+		}
 	}
 	
+	
+}
+
+void findPrimeNumbers(int numbers[10000], int upperLimit){
+	
+	int p = 0;
+	 
+	
+	for(int i=0; i< upperLimit+1; i++){
+		
+		if (numbers[i]==0){
+			break;
+		}
+		
+		p = numbers[i];
+		
+		if( numbers[i]%p == 0){
+			
+	  for(int j = i ; j< upperLimit; j++){
+	  	
+		  numbers[j] = numbers[j+1];
+		  
+		}
+	}
+	}
+	
+}
+
+void displayPrimeNumbers(int numbers[10000], int upperLimit){
+	
+	cout<<"\n\nThe prime numbers are -> ";
+	
+	for(int i = 0 ; i< upperLimit; i++){
+	  	
+		  cout<<numbers[i]<<"_";
+		  
+		}
 	
 }
